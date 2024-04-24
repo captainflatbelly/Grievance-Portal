926a63f3f8e2ebf2a4e3cbabcd50074a75c0d816
harshsingh554 <72743057+harshsingh554@users.noreply.github.com.>
2024-04-25 00:34:14 +0530

    fixed data fetch in staff complaint page

--- staff/resolved.php
+++ staff/resolved.php
@@ -1,21 +1,38 @@
 <?php 
-    require_once '../config.php';
+require_once '../config.php';
 
-	if($conn){
-	?>
-		<script>
-			alert('COMPLAINT RESOLVED successfully !!!');
-		</script>
-	<?php
-	}
-	else{
-		die("No Connection".mysqli_connect_error());
-	}
-	
-	$id= $_GET['id'];
-	
-	$resolvequery = mysqli_query($conn, "UPDATE complaints SET status = 'Resolved' WHERE `complaints`.`C_Id` = $id");
-	// $daquery = mysqli_query($conn, "UPDATE complaints SET Date of resolution = current_timestamp() WHERE `complaints`.`C_Id` = $id");
-	
-	header('location:admin.php');
+// Check if connection is established
+if($conn){
+    // If connection is established, show alert message
+    ?>
+    <script>
+        alert('COMPLAINT RESOLVED successfully !!!');
+    </script>
+    <?php
+} else {
+    // If connection is not established, show error message and terminate script
+    die("No Connection" . mysqli_connect_error());
+}
+
+// Get the complaint ID from the URL parameter
+$id = $_GET['id'];
+
+// Prepare the SQL query with a placeholder for the complaint ID
+$resolvequery = mysqli_prepare($conn, "UPDATE complaints SET status = 'Resolved' WHERE `C_Id` = ?");
+if ($resolvequery) {
+    // Bind the complaint ID parameter to the prepared statement
+    mysqli_stmt_bind_param($resolvequery, "s", $id);
+    
+    // Execute the prepared statement
+    mysqli_stmt_execute($resolvequery);
+
+    // Close the prepared statement
+    mysqli_stmt_close($resolvequery);
+    
+    // Redirect to the admin page
+    header('location: sreso.php');
+} else {
+    // If there's an error in preparing the statement, display an error message
+    echo "Error in preparing the statement: " . mysqli_error($conn);
+}
 ?>
--- staff/stotal.php
+++ staff/stotal.php
@@ -14,8 +14,8 @@
             <a href="../destroy.php" ><button class="logb" >Logout</button></a>
         </div>
         <table class="com-table">
-	        <thead>
-		        <tr>
+            <thead>
+                <tr>
                     <th>Complaint ID</th>
                     <th>Mobile No.</th>
                     <th>Category</th>
@@ -23,52 +23,43 @@
                     <th>Priority</th>
                     <th>Description</th>
                     <th>Time of Registration</th>
-                    <th >Staff</th>
+                    <th>Staff</th>
                     <th colspan="2">Status</th> 
-		        </tr>
-	        </thead>
-	        <tbody>
+                </tr>
+            </thead>
+            <tbody>
                 <?php
-
                     require_once '../config.php';
                     session_start();
-
-                    $em = $_SESSION['name'];
-
+                    $em = $_SESSION['id'];
                     $sql = "SELECT * FROM complaints where staff= '$em' ";
                     $result = mysqli_query($conn,$sql);
                     $num = mysqli_num_rows($result);
-
                     while($row = mysqli_fetch_array($result)){
                 ?>
-                        <tr>
-                            
-                            <td scope="row" class="id"><?php echo $row['C_Id'] ?></td>
-                            <td scope="row" class="tab"><?php echo $row['Mob'] ?></td>
-                            <td scope="row" class="tab"><?php echo $row['Category'] ?></td>
-                            <td scope="row" class="tab"><?php echo $row['Location'] ?></td>
-                            <td scope="row" class="tab"><?php echo $row['Priority'] ?></td>
-                            <td scope="row" class="tab"><?php echo $row['Description'] ?></td>
-                            <td scope="row" class="tab"><?php echo $row['Reg_time'] ?></td>
-                            <td scope="row" class="tab"><?php echo $row['staff'] ?></td>
-                            <td scope="row" class="tab"><?php echo $row['status'] ?></td>
-                            <?php 
-                                if($row['status']=="Resolved"){
-                            ?>
-                                    <td class="tab"><a href="resolved.php?id=<?php echo $row['C_Id'];?>"><button class='alress' >Resolved</button></a></td> 
-                            <?php
-                                }
-                                if($row['status']!="Resolved"){
-                            ?>
-                                <td class="tab"><a href="resolved.php?id=<?php echo $row['C_Id'];?>"><button class='ress' >Resolve</button></a></td> 
-
-
-                            </tr>
-                    <?php
-                                }	
-                        }
-                    ?>
+                    <tr>
+                        <td scope="row" class="id"><?php echo $row['C_Id'] ?></td>
+                        <td scope="row" class="tab"><?php echo $row['Mob'] ?></td>
+                        <td scope="row" class="tab"><?php echo $row['Category'] ?></td>
+                        <td scope="row" class="tab"><?php echo $row['Location'] ?></td>
+                        <td scope="row" class="tab"><?php echo $row['Priority'] ?></td>
+                        <td scope="row" class="tab"><?php echo $row['Description'] ?></td>
+                        <td scope="row" class="tab"><?php echo $row['Reg_time'] ?></td>
+                        <td scope="row" class="tab"><?php echo $row['staff'] ?></td>
+                        <td scope="row" class="tab"><?php echo $row['status'] ?></td>
+                        <?php 
+                            // Check if status is not "Resolved"
+                            if($row['status'] != "resolved"){
+                        ?>
+                            <td class="tab"><a href="resolved.php?id=<?php echo $row['C_Id'];?>"><button class='ress' >Resolve</button></a></td>
+                        <?php
+                            }
+                        ?>
+                    </tr>
+                <?php
+                    }	
+                ?>
             </tbody>
-	    </table>
+        </table>
 </body>
 </html>
