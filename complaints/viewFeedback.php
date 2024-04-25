c6a21fc886af9ba53c841d29d2ef7459133e8cff
0mBudsman <buddhadevom@gmail.com>
2024-04-26 01:59:53 +0530

    css

--- complaints/viewFeedback.php
+++ complaints/viewFeedback.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adminboard</title>
-    <link rel="stylesheet" href="staff.css">
+    <link rel="stylesheet" href="cstyle.css">
     <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 </head>
