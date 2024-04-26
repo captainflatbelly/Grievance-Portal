9f3e89e947f82ecceeb2eb8c76a801c778d83989
0mBudsman <buddhadevom@gmail.com>
2024-04-26 05:45:22 +0530

    idk

--- config.php
+++ config.php
@@ -1,19 +1,9 @@
 <?php
 
 define('DB_SERVER','localhost');
-<<<<<<< HEAD
-
-define('DB_USERNAME','admin');
-define('DB_PASSWORD','password');
-
-define('DB_USERNAME','root');
-
-define('DB_NAME','gportal');
-=======
 define('DB_USERNAME','admin');
  define('DB_PASSWORD','password');
-define('DB_NAME','dbms');
->>>>>>> refs/remotes/origin/main
+define('DB_NAME','gportal');
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME,DB_PASSWORD,DB_NAME);
 
